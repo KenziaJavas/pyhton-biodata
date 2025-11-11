1b4b267b38075b784c4b99873b89475aca51b674
Your Name <you@example.com>
2025-11-11 14:36:46 +0700

    sayang

---
+++ Biodata.py
@@ -0,0 +1,15 @@
+#Biodata diri
+
+#Variabel biodata
+nama = "Kenzia javas niscala putra";
+umur = "15 tahun";
+jenis_kelamin = "Laki-laki";
+sekolah = "Smk telkom jakarta";
+kelas = "X RPL 7";
+
+#Menampilkan Output biodata
+print("nama saya adalah ", nama);
+print("umur saya adalah ", umur);
+print("jenis kelamin saya adalah ", jenis_kelamin);
+print("sekolah saya adalah ", sekolah);
+print("kelas saya adalah ", kelas);
